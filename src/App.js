9776f84f79ffcc99d25281e22a4a13c2d9aed7dd
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2020-11-11 16:18:51 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './stylesheets/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-sm-12 col-lg-6" style={{backgroundColor:"green", height:"100vh"}}>First</div>
+        <div className="col-sm-12 col-lg-6" style={{backgroundColor:"green", height:"100vh"}}>Second</div>
+      </div>
     </div>
   );
 }
